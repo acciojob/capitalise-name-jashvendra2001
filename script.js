709e008bf42872fcb98ea4f862bb7012addc56ac
jashvendra2001 <86592846+jashvendra2001@users.noreply.github.com>
2026-09-06 17:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 
 function upperCase(){
-	let letter=document.querySelector("#fname")
-	letter.value.toUpperCase()
+	let letter=document.getElementById("#fname")
+	letter.value=letter.value.toUpperCase()
 }
 
